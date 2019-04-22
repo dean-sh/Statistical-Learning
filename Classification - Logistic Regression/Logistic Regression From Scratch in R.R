5672dc5d140a02ff0b044d174ccac17a6fce9526
Dean Sh <deanshabi@gmail.com>
2019-04-22 13:28:26 +0300

    Logit Algorithm using gradient descent

---
+++ Classification - Logistic Regression/Logistic Regression From Scratch in R.R
@@ -0,0 +1,90 @@
+#Logistic Regression From scratch
+
+df = read.csv("Datasets/auto.csv")
+
+mgp.mean = mean(df$mpg)
+
+df$binaryMpg = rep(0, length(df$mpg))
+df$binaryMpg[df$mpg>mgp.mean] = 1
+
+df$binaryMpg = factor(df$binaryMpg)
+attach(df)
+
+# The Sigmoid Function
+#####################################
+# z = w0 + w1x1 + w2x2 + ....
+# h = g(x) = 1/(1 + exp(-z))
+
+lin = seq(-5, 5, by = 0.1)
+h = 1/(1 + exp(-lin))
+plot(x = lin, y = h, type = "l",main = "Sigmoid Function", col = "blue")
+
+# The Likelihood Function
+#####################################
+
+#P(Y=y | X=x) = 1 / (1 + exp(y*Theta %*% X))
+#We will use gradient descent to update theta values.
+
+update.Theta = function(theta, d, y, x, n, alpha){
+  #theta(j) <- theta(j-1) - alpha*a/b
+  # a = t(replicate(d,y)) * x
+  a = replicate(d,y, simplify = TRUE) * x
+  
+  # theta.x = t(replicate(n, theta)) * t(x)
+  theta.x = replicate(n, theta) * t(x)
+  
+  theta.x = rowSums(theta.x)
+
+  b = 1 + exp(-y * theta.x)
+  print(dim(a))
+  print(dim(b))
+  theta = theta - alpha*rowSums(a/b)
+  
+  return(theta)
+}
+
+Theta.Optimization = function(theta, x, y, threshold, alpha = 0.2, max_iter = 100){
+  n = ncol(x) #number of features
+  d = length(theta) #number of parameters
+  initial = TRUE
+  number.of.iter = 0
+  new.Theta = theta
+  while(number.of.iter <= max_iter){
+    initial = FALSE
+    previous.Theta = new.Theta
+    new.Theta = update.Theta(new.Theta, d, y, x, n, alpha)
+    theta.difference = sum(abs(new.Theta - previous.Theta))
+    if (theta.difference < threshold) break
+    number.of.iter = number.of.iter + 1
+  }
+  return(new.Theta)  
+}
+
+logistic = function(x, theta){
+  model = sapply(1:ncol(X) , function(index) 1 / (1 + exp(sum(theta * X[,index]))))
+  return(model)
+}
+
+p = ncol(df)-1
+theta = rep(0.1, p) #theta = vector length 1xP
+
+alpha = 0.5
+threshold <- 1
+max_iter <- 100
+
+library(dplyr)
+df = select(df, -c(mpg, name))
+X = model.matrix(binaryMpg~., data = df)[,-1] #We want just the feaures, without the bias term
+#x ~ [n,p]
+n = nrow(X)
+
+y = matrix(df[,p+1])
+y = ifelse(y==0, -1, 1)
+
+dim(y)     # 1 X N
+dim(X)     # N X P
+dim(theta) # 1 X P
+
+finaltheta = Theta.Optimization(theta, X, y, threshold, alpha, max_iter)
+
+predictions = logistic(X, finaltheta)
